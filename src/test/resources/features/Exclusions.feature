02b276796f4d47c1d82e74b0aceee93618d4c03e
Susan Badel <susan.badel@opencastsoftware.com>
2023-12-05 16:51:14 +0000

    Added a basic exclusions journey against scaffolded pages

--- src/test/resources/features/Exclusions.feature
+++ src/test/resources/features/Exclusions.feature
@@ -3,9 +3,14 @@
 Feature: Exclusions Feature
 
   @ZAP
-  Scenario: User can access Exclusions journey
+  Scenario: Moving to a different country exclusions journey
     Given the user accesses the IOSS Exclusions service
     When the user signs in as an Organisation with VRN 100000001 and IOSS Number IM9001234567
-    Then the user is at the beginning of the signed in IOSS Exclusions journey
+    Then the user answers yes on the move-country page
+    And the user selects Hungary on the eu-country page
+    And the user enters today for move-date
+    And the user adds HU12345678 on the tax-number page
+    Then the user is on the check-your-answers page
+
 
 
--- src/test/scala/uk/gov/hmrc/test/ui/pages/CommonPage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/CommonPage.scala
@@ -16,6 +16,8 @@
 
 package uk.gov.hmrc.test.ui.pages
 
+import org.openqa.selenium.By
+import org.openqa.selenium.support.ui.{ExpectedConditions, FluentWait}
 import uk.gov.hmrc.test.ui.conf.TestConfiguration
 
 object CommonPage extends BasePage {
@@ -26,6 +28,44 @@ object CommonPage extends BasePage {
     driver.navigate().to(exclusionsUrl)
 
   def checkJourneyUrl(): Unit =
-    driver.getCurrentUrl should startWith(exclusionsUrl)
+    driver.getCurrentUrl should startWith(s"$exclusionsUrl/move-country")
+
+  def checkUrl(url: String): Unit =
+    driver.getCurrentUrl should startWith(s"${TestConfiguration.url("ioss-exclusions-frontend")}/$url")
+
+  def selectAnswer(data: String): Unit = {
+    data match {
+      case "yes" => driver.findElement(By.id("value")).click()
+      case "no"  => driver.findElement(By.id("value-no")).click()
+      case _     => throw new Exception("Option doesn't exist")
+    }
+    CommonPage.clickContinue()
+  }
+
+  def clickContinue(): Unit =
+//    Awaiting IDs being added to buttons
+//    driver.findElement(By.id("continue")).click()
+    driver.findElement(By.className("govuk-button")).click()
+
+  def selectValueAutocomplete(data: String): Unit = {
+    waitForElement(By.id("value"))
+    enterData("value", data)
+    clickContinue()
+  }
+
+  def waitForElement(by: By) =
+    new FluentWait(driver).until {
+      ExpectedConditions.presenceOfElementLocated(by)
+    }
+
+  def enterData(inputId: String, data: String): Unit =
+    driver.findElement(By.id(inputId)).sendKeys(data)
+
+  def enterDate(day: String, month: String, year: String): Unit = {
+    driver.findElement(By.id("value.day")).sendKeys(day)
+    driver.findElement(By.id("value.month")).sendKeys(month)
+    driver.findElement(By.id("value.year")).sendKeys(year)
+
+  }
 
 }
